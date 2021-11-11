a3678b3e78d09faac5a56f69d366c5d2494f0916
zuhairabid <adamjeehackers@gmail.com>
2021-11-11 23:31:01 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+Calculator = require('./calculator.js')
+
+
+
+// ADD, SUBTRACT, DIVIDE , MULTIPLY two numbers
+
+// var sum = new Calculator().add(2,3)
+// console.log(sum)
+
+// ADD, SUBTRACT, DIVIDE , MULTIPLY lists inside list
+
+// var sumList = new Calculator().addList([[2,4],[3,6]]) 
+// console.log(sumList)
+
+var productList = new Calculator().multiplyList([[4,2], [6,3]] ) 
+console.log(productList)
